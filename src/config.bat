b28ed0fb5956f165d55c485504498ec9e34c2afd
Simon Peter <dn.tlp@gmx.net>
2003-01-04 09:39:24 -0700

    Removed config.bat from CVS, which was there by mistake.

--- src/config.bat
+++
@@ -1,8 +0,0 @@
-rem Standard libraries subdirectory
-set libdir=C:\Dokumente und Einstellungen\Simon\Eigene Dateien\lib
-
-rem Standard headers subdirectory
-set includedir=C:\Dokumente und Einstellungen\Simon\Eigene Dateien\include
-
-rem Standard executables subdirectory
-set bindir=C:\Dokumente und Einstellungen\Simon\Eigene Dateien\bin
